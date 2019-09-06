cbb53ebe36da11398beec5358c5a49e370e30298
Sameer Ahmed <sameerahmed1472@gmail.com>
2019-09-06 19:30:01 +0500

    added a few responsive measures to start testing landing page on mobile devices

--- src/App.js
+++ src/App.js
@@ -6,49 +6,49 @@ import Account from "./Views/Account"
 
 import './App.css';
 
-import { BrowserRouter as Router, Route,NavLink as Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink as Link } from "react-router-dom";
 
 import NavCss from './css/nav.css'
 
 function Nav() {
-    return (
-      <Router>
-        <div class='navContainer'>
-            <ul>
-                <li id="logo">Logo</li>
-                <Link exact activeClassName="activeLink" to="/"><li><i class="fas fa-home icon"></i> Home</li></Link>
-                <Link exact activeClassName='activeLink' to="/account"><li><i class="fas fa-user-circle"></i> My Account</li></Link>
-                <Link exact activeClassName='activeLink' to='/basket'><li><i class="fas fa-shopping-basket icon"></i> Basket</li></Link>
-                <Link exact activeClassName='activeLink' to="/contact"><li><i class="fas fa-phone-alt"></i> Contact</li></Link>
-                <Link exact activeClassName="activeLink" to="/about"><li><i class="fas fa-burn"></i> About Us</li></Link>
-               
-            </ul>
-        </div>
-        <Route path="/" exact component={Popular} />
-      <Route path="/account" exact component={Account}/>
-        </Router>
-    )
-    
+  return (
+    <Router>
+      <div class='navContainer'>
+        <ul>
+          <li id="logo">Logo</li>
+          <Link exact activeClassName="activeLink" to="/"><li><i class="fas fa-home icon"></i> Home</li></Link>
+          <Link exact activeClassName='activeLink' to="/account"><li><i class="fas fa-user-circle"></i> My Account</li></Link>
+          <Link exact activeClassName='activeLink' to='/basket'><li><i class="fas fa-shopping-basket icon"></i> Basket</li></Link>
+          <Link exact activeClassName='activeLink' to="/contact"><li><i class="fas fa-phone-alt"></i> Contact</li></Link>
+          <Link exact activeClassName="activeLink" to="/about"><li><i class="fas fa-burn"></i> About Us</li></Link>
+
+        </ul>
+      </div>
+      <Route path="/" exact component={Popular} />
+      <Route path="/account" exact component={Account} />
+    </Router>
+  )
+
 }
+////// THIS IS FOR MAKING NAVIGATION
 
 
-
-class App extends React.Component{
-constructor(props){
-  super(props);
-}
-  render(){
-    return (
-     <div>
-       
-     <Nav></Nav>
-     <Footer></Footer>
- 
-   </div>
-    )
-    }
-  
+class App extends React.Component {
+  constructor(props) {
+    super(props);
   }
+  render() {
+    return (
+      <div>
+
+        <Nav></Nav>
+        <Footer></Footer>
+
+      </div>
+    )
+  }
+
+}
 
 export default App;
 

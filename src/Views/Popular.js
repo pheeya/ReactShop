e76b8d4287362eebfac749f534ed50a884c3f99d
Sameer Ahmed <sameerahmed1472@gmail.com>
2019-09-06 13:06:37 +0500

    added 'On Sale' section and a timer until the sale ends

--- src/Views/Popular.js
+++ src/Views/Popular.js
@@ -3,13 +3,51 @@ import HomeCss from '../css/home.css'
 import ProductCard from '../Components/ProductCard'
 import candle1 from '../img/candle1.png'
 import candle2 from "../img/candle2.png"
-import candle3 from '../img/candle3.png'
+import candle3 from '../img/candle3.png';
+import saleCandle1 from '../img/saleCandle1.png';
+import saleCandle2 from '../img/saleCandle2.png';
+import saleCandle3 from '../img/saleCandle3.png'
 import Slider from '../Components//Slider'
-function Home(props) {
-    return (
+
+var deadline = new Date('Dec 30, 2020 15:37:25')
+
+
+var deadLine = new Date("Jan 5, 2020 15:37:25").getTime();
+var timeLeft = deadLine - new Date().getTime()
+
+class Home extends React.Component {
+    constructor(props){
+        super(props);
+       
+        this.state={
+            now:new Date().getTime(),
+            days : Math.floor(timeLeft/(1000*60*60*24)),
+            hours : Math.floor((timeLeft%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)),
+            minutes : Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)), 
+            seconds : Math.floor((timeLeft % (1000 * 60)) / 1000)}
+            this.timer = this.timer.bind(this)
+    }
+timer(){
+    timeLeft = deadLine - new Date().getTime();
+    this.setState({    now:new Date().getTime(),
+        days : Math.floor(timeLeft/(1000*60*60*24)),
+        hours : Math.floor((timeLeft%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)),
+        minutes : Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)), 
+        seconds : Math.floor((timeLeft % (1000 * 60)) / 1000)})
+    console.log('hello')
+    console.log(this.state.seconds)
+}
+
+componentDidMount(){
+    
+    setInterval(this.timer,1000)
+}
+
+
+render() {return (
         <div class="home">
             <Slider></Slider>
-            <h2 class="homeCategory">Popular</h2>
+            <h3 class="onlyLeftMargin">Viewed and bought recently</h3>
           <div class="allProductCards">
               <ProductCard title="Love Vanilla"
                content="Made with the finest vanilla. 
@@ -40,8 +78,47 @@ function Home(props) {
 
                
                 </div>  
+                <h3 class="onlyLeftMargin">On Sale</h3>
+                <div class="timer">                                                                                                             
+                <div class = 'timeLeft'>{this.state.days}</div>
+                <div class = 'timeLeft'>{this.state.hours}</div>
+                <div class= 'timeLeft'>{this.state.seconds}</div>
+                </div>
+               <div class="allProductCards">
+                <ProductCard 
+                title="S.T AL"
+                candle={saleCandle1}
+                content="To be honest I don't 
+                know a thing about this candle. But its on sale
+                 so there's that."
+                 price = '20.00'
+                ></ProductCard> 
+                <ProductCard
+                title="Angel Mugler"
+                content="No body wanted to buy this candle and the stock
+                is starting to smell like a decompoosing meerkat so this is now on sale.
+                "
+                price="15.00"
+                candle={saleCandle2}
+                ></ProductCard>  
+
+                <ProductCard
+                candle={saleCandle3}
+                title='Expensive looking candle'
+                content="There's not much to say about this candle,
+                it smells like shit but it looks really expensive and flashy but
+                its actually pretty cheap. Its not on sale though, it just is a cheap candle and
+                 putting this under the Sales section was to lure some moms in.
+                because $20? too expensive. WAS $50 and is now $20? Instant buy.
+                 "
+                 price="20.00"
+                ></ProductCard>
+                </div> 
         </div >
-    );
+    );}
 };
+
+
+
 
 export default Home;
